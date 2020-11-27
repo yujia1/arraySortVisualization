8253e26eda191496b92e648b81f01380d91da8ee
Freemasons <hangxigudeaoqi@gmail.com>
2020-11-27 13:51:21 -0800

    done

---
+++ res/main.js
@@ -0,0 +1,83 @@
+
+// variables of generate, speed and size
+var input_arr = document.getElementById("array_size");
+
+var arr_size = input_arr.value;
+
+var input_gen = document.getElementById("array_generate");
+var input_speed = document.getElementById("array_speed");
+
+// get all algo node
+var algo_butt = document.querySelectorAll(".sort_algorithm button");
+
+// create array
+var div_sizes = [];
+var divs =[]
+var margin_size = 0.1;
+var cont = document.getElementById("array_container"); // parent
+cont.style = "flex-direction:row";
+
+input_gen.addEventListener("click", generate_arr);
+input_arr.addEventListener("input", update_arr);
+
+function generate_arr(){
+    cont.innerHTML="";
+    for(var i = 0; i< arr_size;i++) {
+        div_sizes[i] = Math.floor(Math.random() * 0.5 *(input_arr.max-input_arr.min)) + 10; // random hight of each column
+        divs[i] = document.createElement("div");
+
+        cont.appendChild(divs[i]); // add div under array_container
+        nargin_size = 0.1;
+        divs[i].style=" margin: 0%" + margin_size + "%; background-color:lightyellow ; width:" + (100/arr_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+    }
+};
+
+
+
+function update_arr() {
+    arr_size=input_arr.value;
+    generate_arr();
+}
+
+window.onload = update_arr();
+
+
+for(var i =0; i < algo_butt.length; i++) {
+    algo_butt[i].addEventListener("click", runalgo);
+}
+
+function disable_button() {
+    for(var i = 0; i < algo_butt.length; i++) {
+
+        algo_butt[i].classList = [];
+        algo_butt[i].classList.add("butt_locked");
+
+        algo_butt[i].disable = true;
+        input_arr.disable = true;
+
+        input_gen.disbake = true;
+        input_speed.disable = true;
+    
+    }
+}
+
+function runalgo() {
+    disable_button();
+    this.classList.add("butt_selected");
+    switch(this.innerHTML)
+    {
+        
+        case "Merge":Merge();
+                    
+                        break;
+       
+    }
+}
+
+
+
+
+
+
+
+
---
+++ res/visualization.js
@@ -0,0 +1,48 @@
+
+
+var speed = 1000;
+
+input_speed.addEventListener("input", vis_speed);
+
+function vis_speed(){
+    var arr_speed = input_speed.Value;
+    switch(parseInt(arr_speed)) {
+        case 1: speed = 1;
+            break;
+        case 2: speed = 10;
+            break;
+        case 3: speed = 10;
+            break;
+        case 4: speed = 1000;
+            break;
+        case 1: speed = 10000;
+            break;
+    }
+    delay_time = 10000 / (Math.floor(arr_size / 10) * speed);
+};
+
+var delay_time = delayTime = 10000 / (Math.floor(arr_size / 10) * speed);
+
+var vis_delay =  0;
+
+
+function div_update(cont,height,color)
+{
+    window.setTimeout(function(){
+        var width =  (100/arr_size-(2*margin_size));
+        cont.style=" margin:0% " + margin_size + "%; height:" + height + "%; background-color:" + color + "; width: " + width + "%;";
+    },vis_delay+=delay_time);
+}
+
+function enable_buttons() {
+    window.setTimeout(function() {
+        for(var i =0; i < algo_butt.length; i++) {
+            algo_butt[i].classList = [];
+            algo_butt[i].classList.add("butt_unselected");
+            algo_butt[i].disable = false;
+            input_arr.disable = false;
+            input_gen.disbake = false;
+            input_speed.disable = false;
+        }
+    },vis_delay+=delay_time)
+}
